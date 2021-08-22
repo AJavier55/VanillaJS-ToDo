68c40db482a2724004e74162d552e4f57f3edbfa
AJavier55 <aliciajavier5@gmail.com>
2021-08-21 20:49:01 -0400

    focusing on local storage now

--- app.js
+++ app.js
@@ -40,3 +40,49 @@ function addTodo(e) {
     //attach final Todo
     todoList.appendChild(todoDiv);
 }
+
+function deleteTodo(e) {
+    const item = e.target
+
+    if (item.classList[0] === "trash-btn") {
+        //e.target.parentElemet.remove();
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+
+    //    removeLocalTodos(todo);
+    //    todo.addEventListener("transitioned", e => {
+    //        todo.remove();
+    //    });
+    }
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+        console.log(todo);
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+                case "uncompleted":
+                    if (!todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+        }
+    });
+}
+
+
